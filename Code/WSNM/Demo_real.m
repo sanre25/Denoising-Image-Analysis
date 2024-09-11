95530aff7af14c4674e5f1f0b7580ec2f29b11b8
sanre25 <psandeep21@iitk.ac.in>
2024-09-11 12:20:42 +0530

    update

--- Code/WSNM/Demo_real.m
+++ Code/WSNM/Demo_real.m
@@ -6,12 +6,12 @@
 
     randn('seed', 0);
     N_Img = O_Img + nSig* randn(size(O_Img));                                   %Generate noisy image
-    PSNR  =  csnr( N_Img, O_Img, 0, 0 );
+    PSNR  =  csnr( N_Img, O_Img );
     fprintf( 'Noisy Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', inSig, PSNR );
     
     Par   = ParSet(nSig);   
     E_Img = WSNM_DeNoising( N_Img, O_Img, Par );                                %WNNM denoisng function
-    PSNR  = csnr( O_Img, E_Img, 0, 0 );
+    PSNR  = csnr( O_Img, E_Img);
     
     fprintf( 'Estimated Image: nSig = %2.3f, PSNR = %2.2f \n\n\n', nSig, PSNR );
     imshow(uint8(E_Img.*255));
